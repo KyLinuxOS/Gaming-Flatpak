8525cc7683b7a32683e285ebe3b39031c29a71c2
KyLinuxOS <92610000+KyLinuxOS@users.noreply.github.com>
2022-02-07 16:30:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,64 +12,40 @@ flatpak update --appstream
 
 # program list
 
-Steam() {
-  echo "Installation de steam"
+echo "Installation de steam"
  flatpak install flathub com.valvesoftware.Steam
-}
 
-ProtonGe() {
- echo "Installation de ProtonGe"
+echo "Installation de ProtonGe"
   flatpak install com.valvesoftware.Steam.CompatibilityTool.Proton-GE
-}
 
-PorotonUP-QT() {
- echo "Installation de PorotonUP-QT"
+echo "Installation de PorotonUP-QT"
   flatpak install flathub net.davidotek.pupgui2
-}
 
-Lutris() {
- echo "Installation de Lutris"
+echo "Installation de Lutris"
 flatpak install --user flathub-beta net.lutris.Lutris//beta
-}
 
-Flatseal() {
- echo "Installation de Flatseal"
+echo "Installation de Flatseal"
 flatpak install flathub com.github.tchx84.Flatseal
-}
 
-Mangohud() {
- echo "Installation de Mangohud"
+echo "Installation de Mangohud"
 flatpak install flathub org.freedesktop.Platform.VulkanLayer.MangoHud
-}
 
-Discord() {
- echo "Installation de Discord"
+echo "Installation de Discord"
 flatpak install flathub com.discordapp.Discord
-}
 
-OpenRGB() {
- echo "Installation de OpenRGB"
+echo "Installation de OpenRGB"
 flatpak install flathub org.openrgb.OpenRGB
-}
 
-Spotify() {
- echo "Installation de Spotify"
+echo "Installation de Spotify"
 flatpak install flathub com.spotify.Client
-}
 
-OBSSudio() {
- echo "Installation de OBS Studio"
+echo "Installation de OBS Studio"
 flatpak install flathub com.obsproject.Studio
-}
 
-Kdenlive() {
- echo "Installation de Kdenlive"
+echo "Installation de Kdenlive"
 flatpak install flathub org.kde.kdenlive
-}
 
-GtkStressTesting() {
- echo "Installation de GtkStressTesting"
+echo "Installation de GtkStressTesting"
 flatpak install flathub com.leinardi.gst
-}
 
 echo "### installation complete"
