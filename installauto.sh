ce9bc2164feb1f18a212331f52cce3e2bb2de5ce
KyLinuxOS <92610000+KyLinuxOS@users.noreply.github.com>
2022-02-08 11:10:29 +0100

    Update installauto.sh

--- installauto.sh
+++ installauto.sh
@@ -8,7 +8,7 @@ flatpak remote-add --if-not-exists --user flathub-beta https://flathub.org/beta-
 flatpak update --appstream
 
 #Auto
-flatpak install org.gnome.Platform/x86_64/41 com.valvesoftware.Steam com.valvesoftware.Steam.CompatibilityTool.Proton-GE net.lutris.Lutris//beta net.davidotek.pupgui2 org.freedesktop.Platform.VulkanLayer.MangoHud org.openrgb.OpenRGB com.obsproject.Studio com.spotify.Client org.kde.kdenlive com.leinardi.gst
+flatpak install org.gnome.Platform/x86_64/41 com.valvesoftware.Steam com.valvesoftware.Steam.CompatibilityTool.Proton-GE net.davidotek.pupgui2 org.freedesktop.Platform.VulkanLayer.MangoHud org.openrgb.OpenRGB com.obsproject.Studio com.spotify.Client org.kde.kdenlive com.leinardi.gst
 
 
 echo "### installation complete"
